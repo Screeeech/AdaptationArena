b0486b26cf7d1b032b416fd85123b70141cfd69c
Hasith Vattikuti <hasith@utexas.edu>
2023-12-31 19:08:19 -0600

    split into two files

--- SheepWolfGrass.jl
+++ SheepWolfGrass.jl
@@ -1,3 +1,7 @@
+module SheepWolfGrass
+
+export initialize_model, sheepwolf_step!, grass_step!, Sheep, Wolf
+
 using Agents, Random
 using Distributions
 using GLMakie
@@ -171,58 +175,4 @@ function grass_step!(model)
     end
 end
 
-function plot_population_timeseries(adf, mdf)
-    figure = Figure(resolution = (600, 400))
-    ax = figure[1, 1] = Axis(figure; xlabel = "Step", ylabel = "Population")
-    sheepl = lines!(ax, adf.step, adf.count_sheep, color = :cornsilk4)
-    wolfl = lines!(ax, adf.step, adf.count_wolf, color = RGBAf(0.2, 0.2, 0.3))
-    grassl = lines!(ax, mdf.step, mdf.count_grass, color = :green)
-    figure[1, 2] = Legend(figure, [sheepl, wolfl, grassl], ["Sheep", "Wolves", "Grass"])
-    figure
 end
-
-offset(a) = a isa Sheep ? (-0.1, -0.1*rand()) : (+0.1, +0.1*rand())
-ashape(a) = a isa Sheep ? :circle : :utriangle
-acolor(a) = a isa Sheep ? RGBAf(1.0, 1.0, 1.0, 0.8) : RGBAf(0.2, 0.2, 0.3, 0.8)
-
-grasscolor(model) = model.countdown ./ model.regrowth_time
-heatkwargs = (colormap = [:brown, :green], colorrange = (0, 1))
-
-plotkwargs = (;
-    ac = acolor,
-    as = 25,
-    am = ashape,
-    offset,
-    scatterkwargs = (strokewidth = 1.0, strokecolor = :black),
-    heatarray = grasscolor,
-    heatkwargs = heatkwargs,
-)
-
-stable_params = (;
-    n_sheep = 110,
-    n_wolves = 18,
-    dims = (30, 30),
-    Δenergy_sheep = 5,
-    sheep_reproduce = 0.31,
-    wolf_reproduce = 0.06,
-    Δenergy_wolf = 30,
-    seed = 71758,
-)
-
-sheepwolfgrass = initialize_model(;stable_params...)
-
-#=
-fig, ax, abmobs = abmplot(sheepwolfgrass;
-    agent_step! = sheepwolf_step!,
-    model_step! = grass_step!,
-plotkwargs...)
-fig
-=#
-
-sheep(a) = a isa Sheep
-wolf(a) = a isa Wolf
-count_grass(model) = count(model.fully_grown)
-adata = [(sheep, count), (wolf, count)]
-mdata = [count_grass]
-adf, mdf = run!(sheepwolfgrass, sheepwolf_step!, grass_step!, 2000; adata, mdata)
-plot_population_timeseries(adf, mdf)
